da5632cb3b7fa59eb7278a35fbc3a9cc759fa37d
Cauã Felipe <cauafelipe398@gmail.com>
2023-02-17 13:36:24 -0300

    Finalizado atividade 3

--- appTabela.js
+++ appTabela.js
@@ -23,25 +23,25 @@ entradaDados.question('Digite um número de início entre 0 e 500: ', function (
             function (qualTabela) {
                 let modo = qualTabela.toUpperCase().replace('Í','I');
                 if (modo != 'IMPAR' && modo != 'PAR' && modo != 'AMBOS') {
-                    console.log('ERRO: Esse modo não existe!');
+                    console.log('ERRO: Não foi possivel encontrar esse modo!');
                     entradaDados.close();
                 } else if(valor1 % 1 !== 0 || valor2 % 1 !== 0){
-                    console.log('ERRO: Apenas números inteiros são aceitos!');
+                    console.log('ERRO: Coloque número inteiro!');
                     entradaDados.close();
                 } else if (valor1 == '' || valor2 == '' || modo == '') {
-                    console.log('ERRO: Nenhum dado deve estar vazio!');
+                    console.log('ERRO: Preencha todos os dados!');
                     entradaDados.close();
                 } else if(isNaN(valor1) || isNaN(valor2)){
-                    console.log('ERRO: Os dois valores devem ser números!');
+                    console.log('ERRO: Coloque número!');
                     entradaDados.close();
                 } else if (valor1 < 0 || valor1 > 500 || valor2 < 100 || valor2 > 1000) {
-                    console.log('ERRO: O número inicial deve ser entre 0 e 500 e o número final deve ser entre 100 e 1000.');
+                    console.log('ERRO: Número inicial deve ser de 0 a 500 e o final de 100 a 1000.');
                     entradaDados.close();
                 } else if (parseInt(valor1) > parseInt(valor2)) {
-                    console.log('ERRO: O número inicial não deve ser maior que o número final.');
+                    console.log('ERRO: Número inicial não pode ser maior que o numero final.');
                     entradaDados.close();
                 } else if (valor1 == valor2) {
-                    console.log('ERRO: Os valores digitados não podem ser iguais.');
+                    console.log('ERRO: Os valores não podem ser igual.');
                     entradaDados.close();
                 } else {
                     let geradorTabela = tabela.gerarTabelaImparPar(valor1, valor2, modo);
